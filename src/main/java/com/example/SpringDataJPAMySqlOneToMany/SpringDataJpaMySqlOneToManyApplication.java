ed07c3d04bd5e77606523249437b8efcbb2d7de0
Shafat  Saeed <shafat.saeed@gmail.com>
2019-08-28 21:51:35 +0100

    Initial commit

---
+++ src/main/java/com/example/SpringDataJPAMySqlOneToMany/SpringDataJpaMySqlOneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataJPAMySqlOneToMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMySqlOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaMySqlOneToManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDataJPAMySqlOneToMany/SpringDataJpaMySqlOneToManyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SpringDataJPAMySqlOneToMany;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataJpaMySqlOneToManyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
